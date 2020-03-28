70952f427efa1f6d4b79a38ea6952ff339d54da2
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2020-03-28 16:05:08 +0100

    Add save and load JSON file operations

---
+++ Projekt/SaveAndLoadData.py
@@ -0,0 +1,22 @@
+import json
+
+
+def save_data(data, filename):
+    """
+    :param filename: name of file to save data
+    :param data: data to save to file
+    :return: nothing
+    """
+    with open(filename, 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def load_data(filename):
+    """
+    :param filename: name of json file
+    :return: data from json file
+    """
+    with open(filename) as json_file:
+        data = json.load(json_file)
+
+    return data
