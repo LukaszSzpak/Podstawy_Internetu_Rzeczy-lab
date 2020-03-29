758ebb0f0b7d54fd58f8b943a5061ac847940e5a
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2020-03-29 22:47:49 +0200

    add multithread main

---
+++ Projekt/main.py
@@ -0,0 +1,34 @@
+from Employee import Employee
+from SaveAndLoadData import load_data
+from EntranceGateService import EntranceGateService
+from threading import Thread
+from CommandLineConfig import CommandLineConfig
+
+FILE_NAME = 'list.json'
+
+
+def parse_JSON_to_List(employee_JSON):
+    return [Employee(i['id'], i['name'], i['surname'], i['timesheet']) for i in employee_JSON]
+
+
+def gate_thread(gate):
+    gate.handle_data_from_gate()
+
+
+def config(line_conf):
+    line_conf.program_config()
+
+
+class main:
+    print("Starting program...")
+    employ_list = parse_JSON_to_List(load_data(FILE_NAME))
+    gate = EntranceGateService(employ_list, FILE_NAME)
+    conf = CommandLineConfig(employ_list, FILE_NAME)
+
+    if __name__ == "__main__":
+        thread1 = Thread(target=gate_thread, args=(gate,))
+        thread2 = Thread(target=config, args=(conf,))
+        thread1.start()
+        thread2.start()
+        thread1.join()
+        thread2.join()
