7e46079923463b70ac31673b2553370f1f1feb4f
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2020-03-29 22:46:58 +0200

    add csv writer

--- Projekt/SaveAndLoadData.py
+++ Projekt/SaveAndLoadData.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 
 def save_data(filename, data):
@@ -20,3 +21,20 @@ def load_data(filename):
         data = json.load(json_file)
 
     return data
+
+
+def prepare_and_save(emp_list, file_name):
+    employ = [i.__dict__ for i in emp_list]
+    save_data(file_name, employ)
+
+
+def save_csv(filename, emp_list):
+    fieldnames = ['id', 'name', 'surname', 'last_enter']
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in emp_list:
+            writer.writerow({'id': i.id,
+                             'name': i.name,
+                             'surname': i.surname,
+                             'last_enter': i.timesheet[-1] if i.timesheet != [] else ''})
